93299cca9d7234b4cf39419a005d26d2ad3a81e3
Antonio Ramirez <ramirez.cobos@gmail.com>
2013-04-09 14:35:53 +0200

    add structure

---
+++ backend/controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * SiteController class
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @copyright 2013 2amigOS! Consultation Group LLC
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+class SiteController extends EController
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+}
---
+++ common/config/main.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *
+ * main.php configuration file
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @copyright 2013 2amigOS! Consultation Group LLC
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+return array(
+	'preload' => array('log'),
+	'aliases' => array(
+		'frontend' => dirname(__FILE__) . '/../..' . '/frontend',
+		'common' => dirname(__FILE__) . '/../..' . '/common',
+		'backend' => dirname(__FILE__) . '/../..' . '/backend',
+		'vendor' => 'common.lib.vendor'
+	),
+	'import' => array(
+		'common.extensions.components.*',
+		'common.components.*',
+		'common.helpers.*',
+		'common.models.*',
+		'application.controllers.*',
+		'application.extensions.*',
+		'application.helpers.*',
+		'application.models.*'
+	),
+	'components' => array(
+		'db' => array(
+			'connectionString' => '{CONNECTION_STRING}',
+			'username' => '{USERNAME}',
+			'password' => '{PASSWORD}',
+			'enableProfiling' => YII_DEBUG,
+			'enableParamLogging' => YII_DEBUG,
+			'charset' => 'utf8',
+		),
+		'errorHandler' => array(
+			'errorAction' => 'site/error',
+		),
+		'log' => array(
+			'class'  => 'CLogRouter',
+			'routes' => array(
+				array(
+					'class'        => 'CDbLogRoute',
+					'connectionID' => 'db',
+					'levels'       => 'error, warning',
+				),
+			),
+		),
+	)
+);
