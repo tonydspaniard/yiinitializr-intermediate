e5738f03f17fa591fa180e65a208c59d1a927561
Andrew M <andrew.web@ifdattic.com>
2013-05-01 21:14:46 +0300

    added missing action and views

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -15,4 +15,18 @@ class SiteController extends EController
 	{
 		$this->render('index');
 	}
+  
+  /**
+   * This is the action to handle external exceptions.
+   */
+  public function actionError()
+  {
+    if($error=Yii::app()->errorHandler->error)
+    {
+      if(Yii::app()->request->isAjaxRequest)
+        echo $error['message'];
+      else
+        $this->render('error', $error);
+    }
+  }
 }
