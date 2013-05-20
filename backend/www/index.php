324e44e7968afde1f59729f3b923dbb400b72fcb
Andrew M <andrew.web@ifdattic.com>
2013-05-20 18:23:48 +0300

    Fix application init

--- backend/www/index.php
+++ backend/www/index.php
@@ -10,15 +10,12 @@
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
 require('./../../common/lib/vendor/autoload.php');
-require('./../../common/lib/vendor/yiisoft/yii/framework/yii.php');
 
-Yii::setPathOfAlias('Yiinitializr', './../../common/lib/Yiinitializr');
-
-use Yiinitializr\Helpers\Initializer;
-
-
-Initializer::create('./../', 'backend', array(
+Yiinitializr\Helpers\Initializer::create('./../', 'backend', array(
 	__DIR__ .'/../../common/config/main.php',
 	__DIR__ .'/../../common/config/env.php',
 	__DIR__ .'/../../common/config/local.php',
+	'main',
+  'env',
+  'local'
 ))->run();
